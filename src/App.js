4d88d9de6f5098bf1f852d8d1a8d3b377fba42ae
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2019-09-21 17:47:19 -0400

    Add Promotions route

--- src/App.js
+++ src/App.js
@@ -6,23 +6,29 @@ import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Content from './components/content/Content';
 import Home from './components/content/Home';
-
+import Promotions from './components/content/Promotions';
 
 export const App = () => {
   return (
     <Router>
+      <div>
+
       <Layout>
         <Header>
           {/* Links can go in here */}
+          <Link to="/">Home</Link>
+          <Link to="/promotions">Promotions</Link>
         </Header>
         <Content>
           {/* Routes/Pages can go in here */}
           <Route path="/" exact component={Home} />
+          <Route path="/promotions" exact component={Promotions} />
         </Content>
         <Footer>
           {/* Team Info and external links can go in here */}
         </Footer>
       </Layout>
+      </div>
     </Router>
   );
 }
