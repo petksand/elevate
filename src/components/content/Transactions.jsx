1dc9376946d05b477e12ad2ddfc8eb2b6ee537d5
k-n-sk <41521331+k-n-sk@users.noreply.github.com>
2019-09-21 19:37:53 -0400

    stupid ticket number= multiple cards

--- src/components/content/Transactions.jsx
+++ src/components/content/Transactions.jsx
@@ -1,31 +1,80 @@
 import React from "react";
-import { Card, Icon, Avatar } from 'antd';
+import { Card, Col, Row, Icon, Avatar } from 'antd';
 
 const { Meta } = Card;
 
 class Transactions extends React.Component {
-    render() {
-        return (<Card
-            style={{ width: 300 }}
-            cover={
-              <img
-                alt="example"
-                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+  render() {
+    return (
+      <div style={{ background: '#ECECEC', padding: '30px' }}>
+        <Row gutter={16}>
+          <Col span={8}>
+            <Card
+              style={{ width: 300 }}
+              cover={
+                <img
+                  alt="example"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                />
+              }
+              actions={[
+                <Icon type="qrcode" key="qrcode" />,
+                <Icon type="dollar" key="dollar" />,
+              ]}
+            >
+              <Meta
+                avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                title="Mos Mos"
+                description="This is the description"
               />
-            }
-            actions={[
-              <Icon type="qrcode" key="qrcode" />,
-              <Icon type="dollar" key="dollar" />,
-            ]}
-          >
-            <Meta
-              avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-              title="Mos Mos"
-              description="This is the description"
-            />
-          </Card>
-        );
-    }
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card
+              style={{ width: 300 }}
+              cover={
+                <img
+                  alt="example"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                />
+              }
+              actions={[
+                <Icon type="qrcode" key="qrcode" />,
+                <Icon type="dollar" key="dollar" />,
+              ]}
+            >
+              <Meta
+                avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                title="Mos Mos"
+                description="This is the description"
+              />
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card
+              style={{ width: 300 }}
+              cover={
+                <img
+                  alt="example"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                />
+              }
+              actions={[
+                <Icon type="qrcode" key="qrcode" />,
+                <Icon type="dollar" key="dollar" />,
+              ]}
+            >
+              <Meta
+                avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                title="Mos Mos"
+                description="This is the description"
+              />
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default Transactions;
