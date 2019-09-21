fbe0c89cf3658f689632a5cf90a50683d4e05438
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2019-09-21 18:19:09 -0400

    Remove unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import { Route, BrowserRouter as Router, Link } from 'react-router-dom';
-import { Layout, Button } from 'antd';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Layout } from 'antd';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Content from './components/content/Content';
