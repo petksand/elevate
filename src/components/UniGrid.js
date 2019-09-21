af462e529b77a0a899e95796a85864a45b05fa8e
asleshapokhrel <asleshapokhrel21@gmail.com>
2019-09-21 13:58:40 -0400

    Fixed

--- src/components/UniGrid.js
+++ src/components/UniGrid.js
@@ -5,12 +5,12 @@ import { Box, Grid } from "grommet";
 class UniGrid extends Component {
   render() {
     return (
-      <Grid fill rows={["auto", "flex", "auto"]}>
+      <Grid rows={["auto", "flex", "auto"]}>
         <Box tag="header" background="brand" height="medium">
           Header
         </Box>
         <Box direction="column" justify="center">
-          <Box width="small" background="light-2" width="xxlarge" height="xxlarge">
+          <Box background="light-2" width="xxlarge" height="medium">
             sidebar
           </Box>
           <Box overflow="auto" width="xxlarge" height="xxlarge">
@@ -22,16 +22,7 @@ class UniGrid extends Component {
             <p>feed</p>
             <p>feed</p>
             <p>feed</p>
-            <p>feed</p>
-            <p>feed</p>
-            <p>feed</p>
-            <p>feed</p>
-            <p>feed</p>
-            <p>feed</p>
           </Box>
-        </Box>
-        <Box tag="footer" background="dark-1" height="small">
-          footer
         </Box>
       </Grid>
     );
