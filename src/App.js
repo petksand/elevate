420326bd864808b60d4a34cb022e093d22b51987
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2019-09-21 17:22:09 -0400

    Add router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./App.css";
+import { Route, Router } from 'react-router-dom';
 
-function App() {
+export const App = () => {
   return (
-    <div></div>
+    <Router>
+      
+    </Router>
   );
 }
 
