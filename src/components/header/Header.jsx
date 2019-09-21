4a19e17a05487f01bedb8f876c437a87595e7617
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2019-09-21 17:31:13 -0400

    Scaffold

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Layout } from 'antd';
+
+const { Header: AntHeader } = Layout;
+
+export const Header = () => {
+    return (
+        <AntHeader>
+
+        </AntHeader>
+    );
+};
+
+export default Header;
