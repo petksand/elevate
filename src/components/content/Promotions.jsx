36bedc957a8d9ae899ea420f1c6e8205b86b9a25
Smit Rao <smit.rao@mail.utoronto.ca>
2019-09-21 21:42:31 -0400

    Add flex

--- src/components/content/Promotions.jsx
+++ src/components/content/Promotions.jsx
@@ -14,12 +14,13 @@ class Promotions extends React.Component {
           display: "flex",
           flexWrap: "wrap",
           flexDirection: "row",
-          justifyContent: "space-around",
-          alignItems: "flex-start"
+          justifyContent: "center",
+          alignItems: "center",
+          alignContent: "flex-start"
         }}
       >
         <Card
-          style={{ width: 300, marginTop: 16 }}
+          style={{ width: 300, margin: "1.5rem" }}
           actions={[
             <div>
               <Icon type="info-circle" key="info" />
@@ -36,7 +37,182 @@ class Promotions extends React.Component {
           />
         </Card>
         <Card
-          style={{ width: 300, marginTop: 16 }}
+          style={{ width: 300, margin: "1.5rem" }}
+          actions={[
+            <div>
+              <Icon type="info-circle" key="info" />
+              <Text style={{ padding: "10px" }}>Details</Text>
+            </div>
+          ]}
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3Xsbq68JKdT8INp0JJCBQkclv-NBgC41xp5VakmzuRojsDK4d" />
+            }
+            title="Gymshark"
+            description="Take 70% off your next purchase of $200 or more. Conditions apply; see details for more information."
+          />
+        </Card>
+
+        <Card
+          style={{ width: 300, margin: "1.5rem" }}
+          actions={[
+            <div>
+              <Icon type="info-circle" key="info" />
+              <Text style={{ padding: "10px" }}>Details</Text>
+            </div>
+          ]}
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://www.lafabricadebordados.es/2611-large_default/reebok-logo-embroidered-patch.jpg" />
+            }
+            title="Reebok"
+            description="Take 12% off your next purchase. Conditions apply; see details for more information."
+          />
+        </Card>
+        <Card
+          style={{ width: 300, margin: "1.5rem" }}
+          actions={[
+            <div>
+              <Icon type="info-circle" key="info" />
+              <Text style={{ padding: "10px" }}>Details</Text>
+            </div>
+          ]}
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3Xsbq68JKdT8INp0JJCBQkclv-NBgC41xp5VakmzuRojsDK4d" />
+            }
+            title="Gymshark"
+            description="Take 70% off your next purchase of $200 or more. Conditions apply; see details for more information."
+          />
+        </Card>
+
+        <Card
+          style={{ width: 300, margin: "1.5rem" }}
+          actions={[
+            <div>
+              <Icon type="info-circle" key="info" />
+              <Text style={{ padding: "10px" }}>Details</Text>
+            </div>
+          ]}
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://www.lafabricadebordados.es/2611-large_default/reebok-logo-embroidered-patch.jpg" />
+            }
+            title="Reebok"
+            description="Take 12% off your next purchase. Conditions apply; see details for more information."
+          />
+        </Card>
+        <Card
+          style={{ width: 300, margin: "1.5rem" }}
+          actions={[
+            <div>
+              <Icon type="info-circle" key="info" />
+              <Text style={{ padding: "10px" }}>Details</Text>
+            </div>
+          ]}
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3Xsbq68JKdT8INp0JJCBQkclv-NBgC41xp5VakmzuRojsDK4d" />
+            }
+            title="Gymshark"
+            description="Take 70% off your next purchase of $200 or more. Conditions apply; see details for more information."
+          />
+        </Card>
+
+        <Card
+          style={{ width: 300, margin: "1.5rem" }}
+          actions={[
+            <div>
+              <Icon type="info-circle" key="info" />
+              <Text style={{ padding: "10px" }}>Details</Text>
+            </div>
+          ]}
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://www.lafabricadebordados.es/2611-large_default/reebok-logo-embroidered-patch.jpg" />
+            }
+            title="Reebok"
+            description="Take 12% off your next purchase. Conditions apply; see details for more information."
+          />
+        </Card>
+        <Card
+          style={{ width: 300, margin: "1.5rem" }}
+          actions={[
+            <div>
+              <Icon type="info-circle" key="info" />
+              <Text style={{ padding: "10px" }}>Details</Text>
+            </div>
+          ]}
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3Xsbq68JKdT8INp0JJCBQkclv-NBgC41xp5VakmzuRojsDK4d" />
+            }
+            title="Gymshark"
+            description="Take 70% off your next purchase of $200 or more. Conditions apply; see details for more information."
+          />
+        </Card>
+
+        <Card
+          style={{ width: 300, margin: "1.5rem" }}
+          actions={[
+            <div>
+              <Icon type="info-circle" key="info" />
+              <Text style={{ padding: "10px" }}>Details</Text>
+            </div>
+          ]}
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://www.lafabricadebordados.es/2611-large_default/reebok-logo-embroidered-patch.jpg" />
+            }
+            title="Reebok"
+            description="Take 12% off your next purchase. Conditions apply; see details for more information."
+          />
+        </Card>
+        <Card
+          style={{ width: 300, margin: "1.5rem" }}
+          actions={[
+            <div>
+              <Icon type="info-circle" key="info" />
+              <Text style={{ padding: "10px" }}>Details</Text>
+            </div>
+          ]}
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3Xsbq68JKdT8INp0JJCBQkclv-NBgC41xp5VakmzuRojsDK4d" />
+            }
+            title="Gymshark"
+            description="Take 70% off your next purchase of $200 or more. Conditions apply; see details for more information."
+          />
+        </Card>
+
+        <Card
+          style={{ width: 300, margin: "1.5rem" }}
+          actions={[
+            <div>
+              <Icon type="info-circle" key="info" />
+              <Text style={{ padding: "10px" }}>Details</Text>
+            </div>
+          ]}
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://www.lafabricadebordados.es/2611-large_default/reebok-logo-embroidered-patch.jpg" />
+            }
+            title="Reebok"
+            description="Take 12% off your next purchase. Conditions apply; see details for more information."
+          />
+        </Card>
+        <Card
+          style={{ width: 300, margin: "1.5rem" }}
           actions={[
             <div>
               <Icon type="info-circle" key="info" />
