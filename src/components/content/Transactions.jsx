6c696db24077ed5852fb413b600ea2af9121525f
k-n-sk <41521331+k-n-sk@users.noreply.github.com>
2019-09-22 02:46:46 -0400

    added sample table to update transaction data per company

--- src/components/content/Transactions.jsx
+++ src/components/content/Transactions.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Card, Col, Row, Icon, Avatar } from 'antd';
+import { Card, Col, Row, Icon, Avatar, Table, Divider, Tag } from 'antd';
 import { AppContext } from '../../App.context';
 
 const { Meta } = Card;
@@ -15,6 +15,80 @@ export const Transactions = () => {
       .then(setTransactions);
   }, []);
 
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: text => <a>{text}</a>,
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: 'Tags',
+      key: 'tags',
+      dataIndex: 'tags',
+      render: tags => (
+        <span>
+          {tags.map(tag => {
+            let color = tag.length > 5 ? 'geekblue' : 'green';
+            if (tag === 'loser') {
+              color = 'volcano';
+            }
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </span>
+      ),
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => (
+        <span>
+          <a>Invite {record.name}</a>
+          <Divider type="vertical" />
+          <a>Delete</a>
+        </span>
+      ),
+    },
+  ];
+  
+  const data = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      tags: ['nice', 'developer'],
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      tags: ['loser'],
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+  ];
+
   return (
     <div style={{ background: '#ECECEC', padding: '30px' }}>
       <Row gutter={16}>
@@ -23,7 +97,7 @@ export const Transactions = () => {
             style={{ width: 300 }}
             cover={
               <img
-                alt="example"
+                alt="mos mos image"
                 src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
               />
             }
@@ -82,6 +156,8 @@ export const Transactions = () => {
           </Card>
         </Col>
       </Row>
+      <Divider />
+      <Row><Table columns={columns} dataSource={data} /></Row>
     </div>
   );
 }
