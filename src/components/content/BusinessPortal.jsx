f5ef6b5a1f2100f6f2bec526a735fb2fd32ba117
asleshapokhrel <asleshapokhrel21@gmail.com>
2019-09-21 20:45:48 -0400

    Added form

--- src/components/content/BusinessPortal.jsx
+++ src/components/content/BusinessPortal.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "./BusinessPortal.css";
+import RegistrationForm from "./RegistrationForm"
 
-export const BusinessPortal = () =>{
-    return <div>Hello businesses</div>
-}
+export const BusinessPortal = () => {
+  return (
+    <div>
+      <h1 className="heading" align="center">
+        Business Portal
+      </h1>
+      <div className="form">
+        <RegistrationForm></RegistrationForm>
+      </div>
+      
+    </div>
+  );
+};
 
 export default BusinessPortal;
---
+++ src/components/content/RegistrationForm.jsx
@@ -0,0 +1,237 @@
+import React from "react";
+
+import {
+  Form,
+  Input,
+  Tooltip,
+  Icon,
+  Cascader,
+  Select,
+  Row,
+  Col,
+  Checkbox,
+  Button,
+  AutoComplete,
+} from 'antd';
+
+const { Option } = Select;
+const AutoCompleteOption = AutoComplete.Option;
+
+const promoTypes = [
+  {
+    value: 'Monthly Category',
+    label: 'Monthly expenditure by Category'
+  },
+  {
+    value: 'Monthly Specific',
+    label: 'Monthly expenditure by Specific brand',
+  }
+];
+
+const Month = [
+  {
+    value: 'January',
+    label: 'January'
+  },
+  {
+    value: 'February',
+    label: 'February',
+  }
+];
+
+class RegistrationForm extends React.Component {
+  state = {
+    confirmDirty: false,
+    autoCompleteResult: [],
+  };
+
+constructor(props) {
+  super(props);
+}
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+
+  handleConfirmBlur = e => {
+    const { value } = e.target;
+    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+  };
+
+  compareToFirstPassword = (rule, value, callback) => {
+    const { form } = this.props;
+    if (value && value !== form.getFieldValue('password')) {
+      callback('Two passwords that you enter is inconsistent!');
+    } else {
+      callback();
+    }
+  };
+
+  validateToNextPassword = (rule, value, callback) => {
+    const { form } = this.props;
+    if (value && this.state.confirmDirty) {
+      form.validateFields(['confirm'], { force: true });
+    }
+    callback();
+  };
+
+  handleWebsiteChange = value => {
+    let autoCompleteResult;
+    if (!value) {
+      autoCompleteResult = [];
+    } else {
+      autoCompleteResult = ['.com', '.org', '.net'].map(domain => `${value}${domain}`);
+    }
+    this.setState({ autoCompleteResult });
+  };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const { autoCompleteResult } = this.state;
+
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 8 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 },
+      },
+    };
+    const tailFormItemLayout = {
+      wrapperCol: {
+        xs: {
+          span: 24,
+          offset: 0,
+        },
+        sm: {
+          span: 16,
+          offset: 8,
+        },
+      },
+    };
+    const prefixSelector = getFieldDecorator('prefix', {
+      initialValue: '86',
+    })(
+      <Select style={{ width: 70 }}>
+        <Option value="86">+86</Option>
+        <Option value="87">+87</Option>
+      </Select>,
+    );
+
+    const websiteOptions = autoCompleteResult.map(website => (
+      <AutoCompleteOption key={website}>{website}</AutoCompleteOption>
+    ));
+
+    return (
+      <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+        <Form.Item label="Promotion type">
+        {getFieldDecorator('promoTypes', {
+            initialValue: ['Monthly Category'],
+            rules: [
+              { type: 'array', required: true, message: 'Please select the promotion type!' },
+            ],
+          })(<Cascader options={promoTypes} />)}
+        </Form.Item>
+        <Form.Item label="Month">
+        {getFieldDecorator('Month', {
+            initialValue: ['January'],
+            rules: [
+              { type: 'array', required: true, message: 'Please select the promotion type!' },
+            ],
+          })(<Cascader options={promoTypes} />)}
+        </Form.Item>
+        <Form.Item label="Confirm Password" hasFeedback>
+          {getFieldDecorator('confirm', {
+            rules: [
+              {
+                required: true,
+                message: 'Please confirm your password!',
+              },
+              {
+                validator: this.compareToFirstPassword,
+              },
+            ],
+          })(<Input.Password onBlur={this.handleConfirmBlur} />)}
+        </Form.Item>
+        <Form.Item
+          label={
+            <span>
+              Nickname&nbsp;
+              <Tooltip title="What do you want others to call you?">
+                <Icon type="question-circle-o" />
+              </Tooltip>
+            </span>
+          }
+        >
+          {getFieldDecorator('nickname', {
+            rules: [{ required: true, message: 'Please input your nickname!', whitespace: true }],
+          })(<Input />)}
+        </Form.Item>
+        <Form.Item label="Habitual Residence">
+          {getFieldDecorator('residence', {
+            initialValue: ['zhejiang', 'hangzhou', 'xihu'],
+            rules: [
+              { type: 'array', required: true, message: 'Please select your habitual residence!' },
+            ],
+          })(<Cascader options={promoTypes} />)}
+        </Form.Item>
+        <Form.Item label="Phone Number">
+          {getFieldDecorator('phone', {
+            rules: [{ required: true, message: 'Please input your phone number!' }],
+          })(<Input addonBefore={prefixSelector} style={{ width: '100%' }} />)}
+        </Form.Item>
+        <Form.Item label="Website">
+          {getFieldDecorator('website', {
+            rules: [{ required: true, message: 'Please input website!' }],
+          })(
+            <AutoComplete
+              dataSource={websiteOptions}
+              onChange={this.handleWebsiteChange}
+              placeholder="website"
+            >
+              <Input />
+            </AutoComplete>,
+          )}
+        </Form.Item>
+        <Form.Item label="Captcha" extra="We must make sure that your are a human.">
+          <Row gutter={8}>
+            <Col span={12}>
+              {getFieldDecorator('captcha', {
+                rules: [{ required: true, message: 'Please input the captcha you got!' }],
+              })(<Input />)}
+            </Col>
+            <Col span={12}>
+              <Button>Get captcha</Button>
+            </Col>
+          </Row>
+        </Form.Item>
+        <Form.Item {...tailFormItemLayout}>
+          {getFieldDecorator('agreement', {
+            valuePropName: 'checked',
+          })(
+            <Checkbox>
+              I have read the <a href="">agreement</a>
+            </Checkbox>,
+          )}
+        </Form.Item>
+        <Form.Item {...tailFormItemLayout}>
+          <Button type="primary" htmlType="submit">
+            Register
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  }
+}
+
+const WrappedRegistrationForm = Form.create({ name: 'register' })(RegistrationForm);
+
+
+export default WrappedRegistrationForm;
